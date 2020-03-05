7823ffdd2fb0e3c1455c1d2aac900ad49dc0a713
Jo Janssen <jo.janssen@student.hogent.be>
2020-03-05 12:48:40 +0100

    Add project files.

---
+++ Enalyzer_App/RestService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enalyzer_App
+{
+    public class RestService
+    {
+        HttpClient _client;
+        public RestService()
+        {
+            _client = new HttpClient();
+        }
+
+        public List<string> Items { get; private set; }
+
+        public async Task<List<string>> RefreshDataAsync()
+        {
+            var uri = new Uri("https://localhost:44313/api/taglist");
+            var response = await _client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Items = JsonConvert.DeserializeObject<List<string>>(content);
+            }
+            return Items;
+        }
+    }
+}
---
+++ Enalyzer_App/ViewModel/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Enalyzer.App.ViewModel
+{
+    public class Class1 : INotifyPropertyChanged
+    {
+        DatePicker datePicker = new DatePicker
+        {
+            MinimumDate = new DateTime(2018, 1, 1),
+            MaximumDate = new DateTime(2018, 12, 31),
+            Date = new DateTime(2018, 6, 21)
+        };
+
+        string nameDatabase;
+        string tag;
+        DateTime SelectedDateFrom = new DateTime();
+        DateTime SelectedDateTo = new DateTime();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Command GetTagsCommand { get; }
+
+        public bool FetchingData { get; private set; }
+
+        public string NameDatabase
+        {
+            get { return nameDatabase; }
+            set
+            {
+                nameDatabase = value;
+                OnPropertyChanged(nameof(NameDatabase));
+                OnPropertyChanged(nameof(DisplayMessage));
+            }
+        }
+
+        public string Tag
+        {
+            get { return tag; }
+            set
+            {
+                tag = value;
+                OnPropertyChanged(nameof(Tag));
+                OnPropertyChanged(nameof(DisplayMessage));
+            }
+        }
+
+        public DateTime From
+        {
+            get { return SelectedDateFrom; }
+            set { SelectedDateFrom = value; }
+        }
+
+        public DateTime To
+        {
+            get { return SelectedDateTo; }
+            set { SelectedDateTo = value; }
+        }
+
+        public string DisplayMessage
+        {
+            get
+            {
+                return $"van Databank {NameDatabase}\nTag: {Tag}\n" +
+                $"tussen\n {SelectedDateFrom}\n en \n{SelectedDateTo }";
+            }
+        }
+
+        async Task LookUpValues()
+        {
+            FetchingData = true;
+            await Task.Delay(4000);
+            FetchingData = false;
+            await Application.Current.MainPage.DisplayAlert("Fetching", "Fetching", "Naah");
+        }
+
+    }
+}
+
